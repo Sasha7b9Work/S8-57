191411bdc38ff5287af9215201e0fecd7a80039e
Sasha <Sasha7b9@tut.by>
2018-11-13 22:37:53 +0300

    Командные файлы для компиляции VS-проекта

--- sources/OSC-VS/Device/build.bat
+++ sources/OSC-VS/Device/build.bat
@@ -1,1 +1,9 @@
+del Debug\Device.exe
+
 "c:\Program Files (x86)\Microsoft Visual Studio\2017\Professional\Common7\IDE\devenv.exe" FPGA.vcxproj /Build
+
+if exist Debug\Device.exe (
+Debug\Device.exe
+) else (
+error_message.vbs
+)
--- sources/OSC-VS/Device/reBuild.bat
+++ sources/OSC-VS/Device/reBuild.bat
@@ -1,1 +1,7 @@
 "c:\Program Files (x86)\Microsoft Visual Studio\2017\Professional\Common7\IDE\devenv.exe" FPGA.vcxproj /Rebuild
+
+if exist Debug\Device.exe (
+Debug\Device.exe
+) else (
+error_message.vbs
+)
