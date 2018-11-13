c409fd31b1377ced89d9a8ff7b92f5c8b04ade09
Sasha <Sasha7b9@tut.by>
2018-11-14 00:33:14 +0300

    Рефакторинг

---
+++ sources/Device/src/Hardware/FSMC_437.cpp
@@ -0,0 +1,18 @@
+#include "defines.h"
+#include "FSMC.h"
+#include "Timer.h"
+
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+void FSMC::WriteToFPGA16(uint8 *address, uint16 value)
+{
+    if (mode != ModeFPGA)
+    {
+        ConfigureForFPGA();
+    }
+
+    PAUSE_ON_TICKS(100);    /// \todo Без этой строки замедлен вывод при включённой оптимизации и TBase >= 0.5мс
+
+    *address = (uint8)value;
+    *(address + 1) = (uint8)(value >> 8);
+}
