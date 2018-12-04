04f9ecfcb7965f7873c9ccc3d0c39743b658d8b8
Sasha7b9 <Sasha7b9@tut.by>
2018-12-04 15:38:05 +0300

    Рефакторинг

--- sources/Device/src/log.h
+++ sources/Device/src/log.h
@@ -1,17 +1,20 @@
 #pragma once
+#include "Utils/String.h"
 
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 class Log
 {
 public:
+    static void MessageString(char *message);
     static void Message(char *format, ...);
     static void MessageTrace(char *file, int line, char *format, ...);
 private:
 };
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-#define LOG_WRITE(...)       Log::Message(__VA_ARGS__)
+#define LOG_WRITE(...)      Log::MessageString(String(__VA_ARGS__).CString());
+//#define LOG_WRITE(...)       Log::Message(__VA_ARGS__)
 #define LOG_ERROR_TRACE(...) Log::Message(__VA_ARGS__)
 #define LOG_ERROR(...)       Log::MessageTrace(__FILE__, __LINE__, __VA_ARGS__)
 #define LOG_FUNC_ENTER()     Log::Message("%s %d enter", __FUNCTION__, __LINE__)
