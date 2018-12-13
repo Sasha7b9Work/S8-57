cca4bea70ce795b2defa4912ea30269c2ce5ad55
Sasha7b9 <Sasha7b9@tut.by>
2018-12-13 11:19:21 +0300

    *

--- sources/Device/src/Hardware/VCP.cpp
+++ sources/Device/src/Hardware/VCP.cpp
@@ -1,15 +1,144 @@
 #include "stdafx.h"
+#include "usbd_desc.h"
+#include "usbd_cdc_interface.h"
 #ifndef WIN32
 #include "defines.h"
+#include "Hardware/CPU.h"
 #include "Hardware/VCP.h"
+#include "usbd_desc.h"
+#include "Utils/Math.h"
+#include <stdarg.h>
 #endif
 
 
-/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-bool               VCP::cableIsConnected = false;
-bool               VCP::connectedToUSB = false;
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 USBD_HandleTypeDef VCP::handleUSBD;
 PCD_HandleTypeDef  VCP::handlePCD;
+bool               VCP::cableUSBisConnected = false;
+bool               VCP::connectedToUSB = false;
 
 
-/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+void VCP::Init()
+{
+    USBD_Init(&handleUSBD, &VCP_Desc, 0);
+    USBD_RegisterClass(&handleUSBD, &USBD_CDC);
+    USBD_CDC_RegisterInterface(&handleUSBD, &USBD_CDC_fops);
+    USBD_Start(&handleUSBD);
+} 
+
+//----------------------------------------------------------------------------------------------------------------------------------------------------
+bool VCP::PrevSendingComplete()
+{
+    USBD_CDC_HandleTypeDef *pCDC = (USBD_CDC_HandleTypeDef *)handleUSBD.pClassData;
+    return pCDC->TxState == 0;
+}
+
+//----------------------------------------------------------------------------------------------------------------------------------------------------
+void VCP::SendDataAsynch(uint8 *buffer, int size)
+{
+#define SIZE_BUFFER 64
+    static uint8 trBuf[SIZE_BUFFER];
+
+    size = Math::Min(size, SIZE_BUFFER);
+    while (!PrevSendingComplete())  {};
+    std::memcpy(trBuf, buffer, (uint)size);
+
+    USBD_CDC_SetTxBuffer(&handleUSBD, trBuf, (uint16)size);
+    USBD_CDC_TransmitPacket(&handleUSBD);
+}
+
+//----------------------------------------------------------------------------------------------------------------------------------------------------
+#define SIZE_BUFFER_VCP 256     /// \todo если поставить размер буфера 512, то на ТЕ207 глюки
+static uint8 buffSend[SIZE_BUFFER_VCP];
+static int sizeBuffer = 0;
+
+//----------------------------------------------------------------------------------------------------------------------------------------------------
+void VCP::Flush()
+{
+    if (sizeBuffer)
+    {
+        USBD_CDC_HandleTypeDef *pCDC = (USBD_CDC_HandleTypeDef *)handleUSBD.pClassData;
+        while (pCDC->TxState == 1) {};
+        USBD_CDC_SetTxBuffer(&handleUSBD, buffSend, (uint16)sizeBuffer);
+        USBD_CDC_TransmitPacket(&handleUSBD);
+        while (pCDC->TxState == 1) {};
+    }
+    sizeBuffer = 0;
+}
+
+//----------------------------------------------------------------------------------------------------------------------------------------------------
+void VCP::SendDataSynch(const uint8 *buffer, int size)
+{
+    if (CONNECTED_TO_USB)
+    {
+        USBD_CDC_HandleTypeDef *pCDC = (USBD_CDC_HandleTypeDef *)handleUSBD.pClassData;
+    
+        do 
+        {
+            if (sizeBuffer + size > SIZE_BUFFER_VCP)
+            {
+                int reqBytes = SIZE_BUFFER_VCP - sizeBuffer;
+                LIMITATION(reqBytes, 0, size);
+                while (pCDC->TxState == 1) {};
+                std::memcpy(buffSend + sizeBuffer, (void *)buffer, (uint)reqBytes);
+                USBD_CDC_SetTxBuffer(&handleUSBD, buffSend, SIZE_BUFFER_VCP);
+                USBD_CDC_TransmitPacket(&handleUSBD);
+                size -= reqBytes;
+                buffer += reqBytes;
+                sizeBuffer = 0;
+            }
+            else
+            {
+                std::memcpy(buffSend + sizeBuffer, (void *)buffer, (uint)size);
+                sizeBuffer += size;
+                size = 0;
+            }
+        } while (size);
+    }
+}
+
+//----------------------------------------------------------------------------------------------------------------------------------------------------
+void VCP::SendStringAsynch(char *data)
+{
+    SendDataAsynch((uint8 *)data, (int)std::strlen(data));
+}
+
+//----------------------------------------------------------------------------------------------------------------------------------------------------
+void VCP::SendStringSynch(char *data)
+{
+    SendDataSynch((uint8 *)data, (int)std::strlen(data));
+}
+
+//----------------------------------------------------------------------------------------------------------------------------------------------------
+void VCP::SendFormatStringAsynch(char *format, ...)
+{
+    if (CONNECTED_TO_USB)
+    {
+        static char buffer[200];
+        va_list args;
+        va_start(args, format);
+        std::vsprintf(buffer, format, args);
+        va_end(args);
+        std::strcat(buffer, "\r\n");
+        SendDataAsynch((uint8 *)buffer, (int)std::strlen(buffer));
+    }
+}
+
+//----------------------------------------------------------------------------------------------------------------------------------------------------
+void VCP::SendFormatStringSynch(char *format, ...)
+{
+    char buffer[200];
+    va_list args;
+    va_start(args, format);
+    std::vsprintf(buffer, format, args);
+    va_end(args);
+    std::strcat(buffer, "\r\n");
+    SendDataSynch((uint8 *)buffer, (int)std::strlen(buffer));
+}
+
+//----------------------------------------------------------------------------------------------------------------------------------------------------
+void VCP::SendByte(uint8 byte)
+{
+    SendDataSynch(&byte, 1);
+}
--- sources/Device/src/Hardware/VCP.h
+++ sources/Device/src/Hardware/VCP.h
@@ -1,22 +1,55 @@
 #pragma once
+#include "defines.h"
 #include <usbd_def.h>
 
 
-/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-#define CABLE_USB_IS_CONNECTED  VCP::cableIsConnected
-#define CONNECTED_TO_USB        VCP::connectedToUSB
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
+/** @defgroup VCP
+ *  @brief Virtual Com Port
+ *  @{
+ */
+ 
+#define VCP_FLUSH()                       VCP::Flush()
+#define VCP_SEND_DATA_SYNCH(buffer, size) VCP::SendDataSynch(buffer, size)
+#define CONNECTED_TO_USB                  VCP::connectedToUSB
+#define CABLE_USB_IS_CONNECTED            VCP::cableUSBisConnected
 
-/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 class VCP
 {
 public:
-    /// true, если подключён кабель
-    static bool cableIsConnected;
-    /// true, если есть подключение по USB
-    static bool connectedToUSB;
+    /// Инициализация
+    static void Init();
+
+    static void SendDataAsynch(uint8 *data, int size);
+
+    static void SendDataSynch(const uint8 *data, int size);
+    /// Передаётся строка без завершающего нуля
+    static void SendStringAsynch(char *data);
+    /// Передаётся строка без завершающего нуля
+    static void SendStringSynch(char *data);
+    /// Эта строка передаётся с завершающими символами \\r\\n
+    static void SendFormatStringAsynch(char *format, ...);
+    /// Эта строка передаётся с завершающими символами \\r\\n
+    static void SendFormatStringSynch(char *format, ...);
+
+    static void SendByte(uint8 data);
+
+    static void Flush();
+
+    static USBD_HandleTypeDef handleUSBD;
 
     static PCD_HandleTypeDef handlePCD;
 
-    static USBD_HandleTypeDef handleUSBD;
+    static bool connectedToUSB;
+
+    static bool cableUSBisConnected;
+
+private:
+    static bool PrevSendingComplete();
 };
+
+
+
+/** @}
+ */
