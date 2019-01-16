a767d96449add4f93bd723085622ddf4c30e3b99
Sasha7b9 <Sasha7b9@tut.by>
2019-01-16 11:56:41 +0300

    Device Удаление мусора

--- sources/Device/src/stdafx.h
+++ sources/Device/src/stdafx.h
@@ -31,7 +31,6 @@
 #include "FPGA/FPGA_HAL.h"
 #include "FPGA/FPGA_Math.h"
 #include "FPGA/FPGA_Osci.h"
-#include "FPGA/FPGA_Recorder.h"
 #include "FPGA/FPGA_Settings.h"
 
 #include "Keyboard/DecoderDevice.h"
