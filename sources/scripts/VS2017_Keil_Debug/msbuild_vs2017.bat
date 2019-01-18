bde6d2f587bb1eac32329f0154c9af4e1f7ffb1d
Sasha7b9 <Sasha7b9@tut.by>
2019-01-19 00:18:14 +0300

    *

--- sources/scripts/VS2017_Keil_Debug/msbuild_vs2017.bat
+++ sources/scripts/VS2017_Keil_Debug/msbuild_vs2017.bat
@@ -3,3 +3,5 @@ MSBuild.exe ..\..\generated\Device\Device56.vcxproj -t:rebuild
 set BUILD_STATUS=%ERRORLEVEL%
 if %BUILD_STATUS%==0 echo Build success
 if not %BUILD_STATUS%==0 echo Build failed
+
+MSBuild.exe ..\..\generated\Panel\Panel56.vcxproj -t:rebuild
