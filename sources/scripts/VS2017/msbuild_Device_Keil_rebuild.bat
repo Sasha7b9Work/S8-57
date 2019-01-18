62aded8b1c61ef8d4b2772efb0f5a651629c2527
Sasha7b9 <Sasha7b9@tut.by>
2019-01-18 23:33:16 +0300

    *

--- sources/scripts/VS2017/msbuild_Device_Keil_rebuild.bat
+++ sources/scripts/VS2017/msbuild_Device_Keil_rebuild.bat
@@ -1,1 +1,5 @@
 MSBuild.exe ..\..\generated\Device\Device56.vcxproj -t:rebuild
+@echo off
+set BUILD_STATUS=%ERRORLEVEL%
+if %BUILD_STATUS%==0 echo Build success
+if not %BUILD_STATUS%==0 echo Build failed
--- sources/scripts/VS2017/readme.txt
+++ sources/scripts/VS2017/readme.txt
@@ -5,4 +5,3 @@ msbuild_Device_rebuild.bat
     Собирает проект, сгенерированный cmake_Device_Keil.bat, с помощью msbuild
 
 cmake_and_msbuild_Device_Keil_rebuild.bat
-    
