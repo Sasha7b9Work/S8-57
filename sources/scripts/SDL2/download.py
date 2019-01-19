8e3bf71bed566cd1c3a5d21c58eac53389642ffb
Sasha7b9 <Sasha7b9@tut.by>
2019-01-19 15:38:45 +0300

    *

--- sources/scripts/SDL2/download.py
+++ sources/scripts/SDL2/download.py
@@ -1,13 +1,15 @@
 import requests
 import re
-import console
 from bs4 import BeautifulSoup
 
+#-----------------------------------------------------------------------------------
 def ExtractName(soup):
     return soup.find('div', {'id': 'content'}).find('div', {'class': 'col left'}).find('blockquote').find('a').get('href')
 
 
+#-----------------------------------------------------------------------------------
 def GetNameFile(url):
+    
     # Здесь текст html-файла
     page = requests.get(url).text
     
@@ -21,8 +23,40 @@ def GetNameFile(url):
     
     return file_name
 
+#-----------------------------------------------------------------------------------    
+def GetFile(src, dest):
 
-##################################################################################################
+    # Сколько загружено байт
+    loaded_size = 0
+    
+    # Сюда будем сохранять файл
+    zip_file = open(dest, 'wb')
+    
+    # Даём запрос на чтение файла
+    r = requests.get(src, stream = True)
+    
+    # Узнаём размер файла
+    size_file = int(r.headers['content-length'])
+    
+    # Размер элементарной порции чтения
+    size_chunk = int(size_file / 100.0)
+      
+    print()
+    
+    for chunk in r.iter_content(chunk_size = size_chunk):
+        zip_file.write(chunk)
+        loaded_size += len(chunk)
+        
+        print("\rDownloaded " + str(int(loaded_size / 1024)) + " kb from " + str(int(size_file / 1024)) + " kb = " + str(int(loaded_size / size_file * 100)) + '%', end='')
+        
+    print()
+    
+    zip_file.close();
+
+
+
+
+#-----------------------------------------------------------------------------------
 site = 'https://www.libsdl.org/'
 
 url = site + 'download-2.0.php'
@@ -32,42 +66,9 @@ file_name = GetNameFile(url)
 # Полное имя файла для скачивания
 full_name = site + file_name
 
-# Сюда будем сохранять файл
-zip_file = open("..\\temp\\" + re.split('/', file_name)[1], 'wb')
-
 print('Download file ' + full_name)
 
-# Даём запрос на чтение файла
-r = requests.get(full_name, stream = True)
-
-# Узнаём размер файла
-size_file = int(r.headers['content-length'])
-
-# Размер элементарной порции чтения
-size_chunk = int(size_file / 100.0)
-
-# Сколько загружено байт
-loaded_size = 0
-
-print()
-
-for chunk in r.iter_content(chunk_size = size_chunk):
-    zip_file.write(chunk)
-    loaded_size += len(chunk)
-    
-    print("\rDownload " + str(int(loaded_size / 1024)) + " from " + str(int(size_file / 1024)) + " kb = " + str(int(loaded_size / size_file * 100)) + '%', end='')
-    
-print()
-
-
-## Отсюда будем считывать
-#remote_file = requests.get(full_name)
-#
-## Записываем файл
-#zip_file.write(remote_file.content)
-
-zip_file.close();
-
+GetFile(full_name, "..\\temp\\" + re.split('/', file_name)[1])
 
 
 
