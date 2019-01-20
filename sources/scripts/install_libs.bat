72bc5942ba683d9c7a7f578b48be7b11fa20830f
Sasha7b9 <Sasha7b9@tut.by>
2019-01-20 20:21:07 +0300

    Скрипты загрузки и сборки работают параллельно

--- sources/scripts/install_libs.bat
+++ sources/scripts/install_libs.bat
@@ -3,11 +3,11 @@ rmdir temp /s /q
 @echo Installation SDL2
 
 cd SDL2
-call install_and_build.bat
+start install_and_build.bat
 cd ..
 
 @echo Installation wxWidgets
 
 cd wxWidgets
-call install_and_build.bat
+start install_and_build.bat
 cd ..
