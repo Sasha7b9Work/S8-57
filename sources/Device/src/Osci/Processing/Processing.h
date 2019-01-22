1543e034becf7974b5ed1c927a3b91c2d9f37157
Sasha7b9 <Sasha7b9@tut.by>
2019-01-22 16:47:15 +0300

    Device Рефакторинг

--- sources/Device/src/Osci/Processing/Processing.h
+++ sources/Device/src/Osci/Processing/Processing.h
@@ -2,31 +2,16 @@
 
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-class Processing
+namespace Processing
 {
-    friend class Measure;
-
-public:
     /// Аппроксимировать единичное измерение режима рандомизатора функцией sinX/X
-    static void InterpolationSinX_X(uint8 *data, int numPoints, Osci::Settings::TBase::E tBase);
+    void InterpolationSinX_X(uint8 *data, int numPoints, Osci::Settings::TBase::E tBase);
     /// Возвращает строку автоматического измерения
-    static String GetStringMeasure(Measure::Type::E measure, Chan::E ch, char *buffer, int lenBuf);
+    String GetStringMeasure(Measure::Type::E measure, Chan::E ch, char *buffer, int lenBuf);
     /// Расчитать все автоматические измерения
-    static void CalculateMeasures();
+    void CalculateMeasures();
 
-    static int markerTime[Chan::Size][2];
+    extern int markerTime[Chan::Size][2];
 
-    static int markerVoltage[Chan::Size][2];
-
-private:
-    /// Приведение сигнала к установленным в приборе настройкам
-    static void CountedToCurrentSettings();
-    /// Приведение сигнала к текущим Range и rShift
-    static void CountedRange(Chan::E ch);
-    /// Приведение сигнала к текущему TBase
-    static void CountedTBase();
-    /// Приведение сигнала к текущему tShift
-    static void CountedTShift();
-    /// Приведение количества точек в сигнале к установленным настройкам
-    static void CountedEnumPoints();
+    extern int markerVoltage[Chan::Size][2];
 };
