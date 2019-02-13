85188326db97c8cf1ad9bdeb7639a590bab170e7
Sasha7b9 <Sasha7b9@tut.by>
2019-02-13 20:03:54 +0300

    *

--- sources/Device/src/Settings/Settings_stm.cpp
+++ sources/Device/src/Settings/Settings_stm.cpp
@@ -1,11 +1,19 @@
 #include "defines.h"
-#include "Settings/Settings.h"
+#include "Settings.h"
 #include "Hardware/Memory.h"
 #include "FPGA/FPGA.h"
 #include "FPGA/FPGA_Settings.h"
 #include "Osci/Osci.h"
-
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-void Settings::Load(bool)
+void Settings::Load(bool _default)
 {
+    if(_default || !Memory::LoadSettings())
+    {
+        Reset();
+        Osci::Init();
+        uint8 data[1024];
+        TransformFromLoad(data);
+        Menu::Init();
+        FPGA::OnPressStart();
+    }
 }
