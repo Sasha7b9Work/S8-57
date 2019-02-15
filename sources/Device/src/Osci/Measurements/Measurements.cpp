31888d9c71f4545df2b03260f94afa9506da5ea0
Sasha7b9 <Sasha7b9@tut.by>
2019-02-15 23:52:16 +0300

    Скипт сбоки GUI-версии рефакторинг

--- sources/Device/src/Osci/Measurements/Measurements.cpp
+++ sources/Device/src/Osci/Measurements/Measurements.cpp
@@ -1454,6 +1454,11 @@ bool Osci::Measurements::DataIsSetting()
 void Osci::Measurements::SetData()
 {
     isSet = (DATA != nullptr);
+
+    if (!isSet)
+    {
+        return;
+    }
  
     CountedToCurrentSettings();
 
