5c0461679cd749cf126a2551463a4aada296308a
Sasha7b9 <Sasha7b9@tut.by>
2019-08-27 17:02:24 +0300

    Меню заработало, но при открытии ДИСПЛЕЙ-НАСТРОЙКИ ВИСНЕТ

--- sources/Device/src/Menu/MenuItemsDefs.h
+++ sources/Device/src/Menu/MenuItemsDefs.h
@@ -25,53 +25,53 @@ static const GovernorColor name(th##name, &colorType, keeper, 0, 0);
 #define DEF_PAGE_1(name, title, hint, item1, namePage, keeper, funcActive, funcPress, funcDraw, funcRegSet)                                                                                 \
 static const Item * const i##name[] = {(Item *)item1};                                                                                                                                      \
 static const char * th##name[] = {title, hint};                                                                                                                                             \
-static const Page name(th##name, keeper, i##name, 1, funcActive, funcPress, funcDraw, funcRegSet);
+static const Page name(namePage, th##name, keeper, i##name, 1, funcActive, funcPress, funcDraw, funcRegSet);
 
 #define DEF_PAGE_2(name, title, hint, item1, item2, namePage, keeper, funcActive, funcPress, funcDraw, funcRegSet)                                                                          \
 static const Item * const  i##name[] = {(Item *)item1, (Item *)item2};                                                                                                                      \
 static const char *th##name[] = {title, hint};                                                                                                                                              \
-static const Page name(th##name, keeper, i##name, 2, funcActive ,funcPress, funcDraw, funcRegSet);
+static const Page name(namePage, th##name, keeper, i##name, 2, funcActive ,funcPress, funcDraw, funcRegSet);
 
 #define DEF_PAGE_3(name, title, hint, item1, item2, item3, namePage, keeper, funcActive, funcPress, funcDraw, funcRegSet)                                                                   \
 static const Item * const i##name[] = {(Item *)(item1), (Item *)(item2), (Item *)(item3)};                                                                                                  \
 static const char *th##name[] = {title, hint};                                                                                                                                              \
-static const Page name(th##name, keeper, i##name, 3, funcActive, funcPress, funcDraw, funcRegSet);
+static const Page name(namePage, th##name, keeper, i##name, 3, funcActive, funcPress, funcDraw, funcRegSet);
 
 #define DEF_PAGE_4(name, title, hint, item1, item2, item3, item4, namePage, keeper, funcActive, funcPress, funcDraw, funcRegSet)                                                            \
 static const Item * const i##name[] = {(Item *)item1, (Item *)item2, (Item *)item3, (Item *)item4};                                                                                         \
 static const char *th##name[] = {title, hint};                                                                                                                                              \
-static const Page name(th##name, keeper, i##name, 4, funcActive, funcPress, funcDraw, funcRegSet);
+static const Page name(namePage, th##name, keeper, i##name, 4, funcActive, funcPress, funcDraw, funcRegSet);
 
 #define DEF_PAGE_5(name, title, hint,  item1, item2, item3, item4, item5, namePage, keeper, funcActive, funcPress, funcDraw, funcRegSet)                                                    \
 static const Item * const i##name[] = {(Item *)item1, (Item *)item2, (Item *)item3, (Item *)item4, (Item *)item5};                                                                          \
 static const char *th##name[] = {title, hint};                                                                                                                                              \
-static const Page name(th##name, keeper, i##name, 5, funcActive ,funcPress, funcDraw, funcRegSet);
+static const Page name(namePage, th##name, keeper, i##name, 5, funcActive ,funcPress, funcDraw, funcRegSet);
 
 #define DEF_PAGE_5_VAR(name, title, hint, item1, item2, item3, item4, item5, namePage, keeper, funcActive, funcPress, funcDraw, funcRegSet)                                                 \
 static const Item *i##name[] = {(Item *)item1, (Item *)item2, (Item *)item3, (Item *)item4, (Item *)item5};                                                                                 \
 static const char *th##name[] = {title, hint};                                                                                                                                              \
-static const Page name(th##name, keeper, i##name, 5, funcActive ,funcPress, funcDraw, funcRegSet);
+static const Page name(namePage, th##name, keeper, i##name, 5, funcActive ,funcPress, funcDraw, funcRegSet);
 
 #define DEF_PAGE_6(name, title, hint, item1, item2, item3, item4, item5, item6, namePage, keeper, funcActive, funcPress, funcDraw, funcRegSet)                                              \
 static const Item * const i##name[] = {(Item *)item1, (Item *)item2, (Item *)item3, (Item *)item4, (Item *)item5, (Item *)item6};                                                           \
 static const char * const th##name[] = {title, hint};                                                                                                                                       \
-static const Page name(th##name, keeper, i##name, 6, funcActive, funcPress, funcDraw, funcRegSet);
+static const Page name(namePage, th##name, keeper, i##name, 6, funcActive, funcPress, funcDraw, funcRegSet);
 
 #define DEF_PAGE_7(name, title, hint, item1, item2, item3, item4, item5, item6, item7, namePage, keeper, funcActive, funcPress, funcDraw, funcRegSet)                                       \
 static const Item * const  i##name[] = { (Item *)item1, (Item *)item2, (Item *)item3, (Item *)item4, (Item *)item5, (Item *)item6, (Item *)item7};                                          \
 static const char * const th##name[] = {title, hint};                                                                                                                                       \
-static const Page name(th##name, keeper, i##name, 7, funcActive, funcPress, funcDraw, funcRegSet);
+static const Page name(namePage, th##name, keeper, i##name, 7, funcActive, funcPress, funcDraw, funcRegSet);
 
 #define DEF_PAGE_8(name, title, hint, item1, item2, item3, item4, item5, item6, item7, item8, namePage, keeper, funcActive, funcPress, funcDraw, funcRegSet)                                \
 static const Item * const  i##name[] = {(Item *)item1, (Item *)item2, (Item *)item3, (Item *)item4, (Item *)item5, (Item *)item6, (Item *)item7, (Item *)item8};                            \
 static const char *th##name[] = {title, hint};                                                                                                                                              \
-static const Page name(th##name, keeper, &i##name[0], 8, funcActive, funcPress, funcDraw, funcRegSet);
+static const Page name(namePage, th##name, keeper, &i##name[0], 8, funcActive, funcPress, funcDraw, funcRegSet);
 
 #define DEF_PAGE_12(name, title, hint, item1, item2, item3, item4, item5, item6, item7, item8, item9, item10, item11, item12, namePage, keeper, funcActive, funcPress, funcDraw, funcRegSet)\
 static const Item * const  i##name[] = {(Item *)item1, (Item *)item2, (Item *)item3, (Item *)item4, (Item *)item5,                                                                          \
     (Item *)item6, (Item *)item7, (Item *)item8, (Item *)item9, (Item *)item10, (Item *)item11, (Item *)item12};                                                                            \
 static const char *th##name[] = {title, hint};                                                                                                                                              \
-static const Page name(th##name, keeper, i##name, 12, funcActive, funcPress, funcDraw, funcRegSet);
+static const Page name(namePage, th##name, keeper, i##name, 12, funcActive, funcPress, funcDraw, funcRegSet);
 
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
