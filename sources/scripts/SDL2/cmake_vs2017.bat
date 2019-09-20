9c0a99862893233706b7837825b3aedea894d154
Sasha7b9 <Sasha7b9@tut.by>
2019-09-20 12:18:33 +0300

    Переход на Visual Studio 2019

--- sources/scripts/SDL2/cmake_vs2017.bat
+++ sources/scripts/SDL2/cmake_vs2017.bat
@@ -1,1 +1,1 @@
-cmake ..\..\ThirdParty\SDL2\SDL2-2.0.10 -B..\..\ThirdParty\SDL2\SDL2-2.0.10\generated -DCMAKE_SYSTEM_VERSION=10.0.17763.0 -DCMAKE_GENERATOR="Visual Studio 15 2017"
+cmake ..\..\ThirdParty\SDL2\SDL2-2.0.10 -B..\..\ThirdParty\SDL2\SDL2-2.0.10\generated -DCMAKE_SYSTEM_VERSION=10.0.17763.0 -G "Visual Studio 16 2019" -A Win32
--- sources/scripts/wxWidgets/cmake_vs2017.bat
+++ sources/scripts/wxWidgets/cmake_vs2017.bat
@@ -1,1 +1,1 @@
-cmake ..\..\ThirdParty\wxWidgets\CMakeLists.txt -B..\..\ThirdParty\wxWidgets\generated -DCMAKE_SYSTEM_VERSION=10.0.17763.0 -DCMAKE_GENERATOR="Visual Studio 15 2017"
+cmake ..\..\ThirdParty\wxWidgets\CMakeLists.txt -B..\..\ThirdParty\wxWidgets\generated -DCMAKE_SYSTEM_VERSION=10.0.17763.0 -G "Visual Studio 16 2019" -A Win32
