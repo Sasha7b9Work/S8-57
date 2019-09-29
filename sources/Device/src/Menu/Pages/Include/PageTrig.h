bd87bf14b63ab0a47315a2c367223b013346c5de
Sasha7b9 <Sasha7b9@tut.by>
2019-09-29 12:17:28 +0300

    Рефакторинг

--- sources/Device/src/Menu/Pages/Include/PageTrig.h
+++ sources/Device/src/Menu/Pages/Include/PageTrig.h
@@ -2,6 +2,20 @@
 #include "Menu/MenuItems.h"
 
 
+struct SettingsTrig
+{
+    Osci::Settings::Trig::Source::E    source;
+    Osci::Settings::Trig::Input::E     input;
+    Osci::Settings::Trig::Polarity::E  polarity;
+    uint16                             lev[Chan::Size];
+    Osci::Settings::Trig::StartMode::E startMode;           ///< Режим запуска.
+    Osci::Settings::Trig::ModeFind::E  modeFind;            ///< Поиск синхронизации - вручную или автоматически.
+                                            /// \todo разрядности trig_holdOff недостаточно
+    int16                              holdOff;             ///< Удержание синхронизации
+    bool                               holdOffEnabled;      ///< Включение удержания синхронизации
+};
+
+
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 struct PageTrig
 {
--- sources/Device/src/Settings/Settings.h
+++ sources/Device/src/Settings/Settings.h
@@ -19,6 +19,7 @@
 #include "Menu/Pages/Include/PageDisplay.h"
 #include "Menu/Pages/Include/PageFunction.h"
 #include "Menu/Pages/Include/PageTime.h"
+#include "Menu/Pages/Include/PageTrig.h"
 
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -44,19 +45,6 @@
 #pragma pack(push, 1)
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-struct SettingsTrig
-{
-    Osci::Settings::Trig::Source::E    source;
-    Osci::Settings::Trig::Input::E     input;
-    Osci::Settings::Trig::Polarity::E  polarity;
-    uint16                             lev[Chan::Size];
-    Osci::Settings::Trig::StartMode::E startMode;           ///< Режим запуска.
-    Osci::Settings::Trig::ModeFind::E  modeFind;            ///< Поиск синхронизации - вручную или автоматически.
-                                            /// \todo разрядности trig_holdOff недостаточно
-    int16                              holdOff;             ///< Удержание синхронизации
-    bool                               holdOffEnabled;      ///< Включение удержания синхронизации
-};
-
 struct SettingsCursors
 {
     bool                                     showCursors;             ///< Показывать ли курсоры.
