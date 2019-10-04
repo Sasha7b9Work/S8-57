ab2c258f44042a37fc6d364791c1733c2f513c2c
Sasha7b9 <Sasha7b9@tut.by>
2019-10-04 16:46:24 +0300

    Рефакторинг

--- sources/Device/src/FPGA/MathFPGA.h
+++ sources/Device/src/FPGA/MathFPGA.h
@@ -2,25 +2,25 @@
 #include "Settings/SettingsOsci.h"
 
 
-namespace MathFPGA
+struct MathFPGA
 {
-    float RShift2Abs(int rShift, Range::E range);
+    static float RShift2Abs(int rShift, Range::E range);
 
-    float VoltageCursor(float shiftCurU, Range::E range, uint16 rShift);
+    static float VoltageCursor(float shiftCurU, Range::E range, uint16 rShift);
 
-    int RShift2Rel(float rShixftAbs, Range::E range);
+    static int RShift2Rel(float rShixftAbs, Range::E range);
 
-    float TimeCursor(float shiftCurT, TBase::E tBase);
+    static float TimeCursor(float shiftCurT, TBase::E tBase);
 
-    void PointsRel2Voltage(const uint8 *points, int numPoints, Range::E range, int16 rShift, float *voltage);
+    static void PointsRel2Voltage(const uint8 *points, int numPoints, Range::E range, int16 rShift, float *voltage);
 
-    uint8 Voltage2Point(float voltage, Range::E range, uint16 rShift);
+    static uint8 Voltage2Point(float voltage, Range::E range, uint16 rShift);
 
-    float Point2Voltage(uint8 value, Range::E range, uint16 rShift);
+    static float Point2Voltage(uint8 value, Range::E range, uint16 rShift);
 
-    void PointsVoltage2Rel(const float *voltage, int numPoints, Range::E range, int16 rShift, uint8 *points);
+    static void PointsVoltage2Rel(const float *voltage, int numPoints, Range::E range, int16 rShift, uint8 *points);
 
-    void CalculateFFT(float *data, int numPoints, float *result, float *freq0, float *density0, float *freq1, float *density1, int *y0, int *y1);
+    static void CalculateFFT(float *data, int numPoints, float *result, float *freq0, float *density0, float *freq1, float *density1, int *y0, int *y1);
 
-    float TShift2Abs(int tShift, TBase::E tBase);
-}
+    static float TShift2Abs(int tShift, TBase::E tBase);
+};
