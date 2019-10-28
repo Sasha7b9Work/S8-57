a780b8a54f1651c31a3fa97460969966bf4682b6
Sasha7b9 <Sasha7b9@tut.by>
2019-10-28 22:34:01 +0300

    vs_win Неудачная попытка исправить баг в регистраторе

--- sources/Device/src/Hardware/Memory.h
+++ sources/Device/src/Hardware/Memory.h
@@ -2,31 +2,31 @@
 #include "Utils/String.h"
 
 
-#define S00            ((uint)0x08000000)	//  16k
-#define S01            ((uint)0x08004000)	//  16k
-#define S02            ((uint)0x08008000)	//  16k
-#define S03            ((uint)0x0800C000)	//  16k
-#define S04            ((uint)0x08010000)	//  64k
-#define S05            ((uint)0x08020000)	// 128k
-#define S06            ((uint)0x08040000)	// 128k
-#define S07            ((uint)0x08060000)	// 128k
-#define S08            ((uint)0x08080000)	// 128k
-#define S09            ((uint)0x080A0000)	// 128k
+#define S00_BOOT_1     ((uint)0x08000000)   //  16k
+#define S01_BOOT_2     ((uint)0x08004000)   //  16k
+#define S02            ((uint)0x08008000)   //  16k
+#define S03            ((uint)0x0800C000)   //  16k
+#define S04            ((uint)0x08010000)   //  64k
+#define S05_FIRM_1     ((uint)0x08020000)   // 128k
+#define S06_FIRM_2     ((uint)0x08040000)   // 128k
+#define S07_FIRM_3     ((uint)0x08060000)   // 128k
+#define S08            ((uint)0x08080000)   // 128k
+#define S09            ((uint)0x080A0000)   // 128k
 /// ѕервый сектор дл€ сохранени€ настроек. ѕри его заполнении начинает использоватьс€ сектор 2.
-#define S10_SETTINGS_1 ((uint)0x080C0000)	// 128k
-#define S11            ((uint)0x080E0000)	// 128k
-#define S12            ((uint)0x08100000)	//  16k
-#define S13            ((uint)0x08104000)	//  16k
-#define S14            ((uint)0x08108000)	//  16k
-#define S15            ((uint)0x0810C000)	//  16k
-#define S16            ((uint)0x08110000)	//  64k
-#define S17_RECORDER_1 ((uint)0x08120000)	// 128k
-#define S18_RECORDER_2 ((uint)0x08140000)	// 128k
-#define S19            ((uint)0x08160000)	// 128k
-#define S20            ((uint)0x08180000)	// 128k
-#define S21            ((uint)0x081A0000)	// 128k
-#define S22            ((uint)0x081C0000)	// 128k
-#define S23            ((uint)0x081E0000)	// 128k
+#define S10_SETTINGS_1 ((uint)0x080C0000)   // 128k
+#define S11_SETTINGS_2 ((uint)0x080E0000)   // 128k
+#define S12            ((uint)0x08100000)   //  16k
+#define S13            ((uint)0x08104000)   //  16k
+#define S14            ((uint)0x08108000)   //  16k
+#define S15            ((uint)0x0810C000)   //  16k
+#define S16            ((uint)0x08110000)   //  64k
+#define S17_RECORDER_1 ((uint)0x08120000)   // 128k
+#define S18_RECORDER_2 ((uint)0x08140000)   // 128k
+#define S19            ((uint)0x08160000)   // 128k
+#define S20            ((uint)0x08180000)   // 128k
+#define S21            ((uint)0x081A0000)   // 128k
+#define S22            ((uint)0x081C0000)   // 128k
+#define S23            ((uint)0x081E0000)   // 128k
 
 
 struct DataSettings;
--- sources/Device/src/Recorder/Recorder.cpp
+++ sources/Device/src/Recorder/Recorder.cpp
@@ -100,7 +100,7 @@ void Recorder::Start()
     RShift::Set(Chan::A, RShift::ZERO);
     RShift::Set(Chan::B, RShift::ZERO);
 
-    EEPROM::EraseSector(S10_SETTINGS_1);
+    EEPROM::EraseSector(S17_RECORDER_1);
 
     StorageRecorder::CreateNewRecord();
 
