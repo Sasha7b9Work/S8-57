18dd95f44d92be00e2446b36e3735bb8d8df2220
Sasha7b9 <Sasha7b9@tut.by>
2019-11-06 10:38:26 +0300

    Заготовка для HAL_TIM4

--- sources/Device/src/Test/Test_Display.cpp
+++ sources/Device/src/Test/Test_Display.cpp
@@ -1,14 +1,21 @@
 #include "defines.h"
 #include "Test/Test.h"
+#include "Hardware/HAL/HAL.h"
 
 
-void Test::Display::Init()
+static void Update()
 {
 
 }
 
 
+void Test::Display::Init()
+{
+    HAL_TIM4::Init(100, Update);
+}
+
+
 void Test::Display::DeInit()
 {
-
+    HAL_TIM4::DeInit();
 }
---
+++ sources/VS/Device/src/Hardware/HAL/HAL_TIM4_win.cpp
@@ -0,0 +1,14 @@
+#include "defines.h"
+#include "Hardware/HAL/HAL.h"
+
+
+void HAL_TIM4::Init(uint, void (*)())
+{
+
+}
+
+
+void HAL_TIM4::DeInit()
+{
+
+}
