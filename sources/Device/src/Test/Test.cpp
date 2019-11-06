d1eeaa428d574e34e44764e9880b726cacdd3eca
Sasha7b9 <Sasha7b9@tut.by>
2019-11-06 08:29:57 +0300

    Рефакторинг

--- sources/Device/src/Test/Test.cpp
+++ sources/Device/src/Test/Test.cpp
@@ -1,11 +1,5 @@
 #include "defines.h"
-#include "Data/DataSettings.h"
-#include "Data/Heap.h"
-#include "Hardware/Memory/Memory.h"
-#include "Hardware/Memory/Sector.h"
-#include "Menu/Pages/Include/PageMemory.h"
 #include "Test/Test.h"
-#include <cstdlib>
 
 
 bool Test::Run()
@@ -13,88 +7,4 @@ bool Test::Run()
     FlashMemory::Data::Test();
     
     return false;
-}
-
-
-namespace Test
-{
-    namespace FlashMemory
-    {
-        namespace Data
-        {
-            static void FillData(uint8 *data, uint numPoints)
-            {
-                for (uint i = 0; i < numPoints; i++)
-                {
-                    data[i] = static_cast<uint8>(i);
-                }
-            }
-
-            static bool Compare(uint8 *src, uint8 *dest, uint numPoints)
-            {
-                for (uint i = 0; i < numPoints; i++)
-                {
-                    if (*src++ != *dest++)
-                    {
-                        return false;
-                    }
-                }
-
-                return true;
-            }
-
-            static void PrepareDS(DataSettings *ds)
-            {
-                uint8 *dataA = static_cast<uint8 *>(Heap::Begin());
-                uint8 *dataB = static_cast<uint8 *>(Heap::Begin() + ds->SizeChannel());
-
-                FillData(dataA, ds->SizeChannel());
-                FillData(dataB, ds->SizeChannel());
-
-                ds->Fill(dataA, dataB);
-
-                ds->enumPoints = static_cast<uint>(std::rand() % ENumPointsFPGA::Count);
-                ds->peackDet = PeakDetMode::Disabled;
-            }
-        }
-    }
-}
-
-
-bool Test::FlashMemory::Data::Test()
-{
-    static uint totalMemory = 0;
-
-    ::FlashMemory::Data::EraseAll();
-
-    for (int i = 0; i < 10000; i++)
-    {
-        DataSettings ds;
-
-        PrepareDS(&ds);
-
-        uint numInROM = std::rand() % ::FlashMemory::Data::MAX_NUM_SAVED_WAVES;
-
-        ::FlashMemory::Data::Save(numInROM, &ds);
-
-        totalMemory += sizeof(Packet) + sizeof(DataSettings) + ds.SizeChannel() + ds.SizeChannel();
-
-        DataSettings *dsRead = nullptr;
-
-        ::FlashMemory::Data::Read(numInROM, &dsRead);
-
-        if (!Compare(ds.dataA, dsRead->dataA, ds.SizeChannel()))
-        {
-            return false;
-        }
-
-        if (!Compare(ds.dataB, dsRead->dataB, ds.SizeChannel()))
-        {
-            return false;
-        }
-
-        continue;
-    }
-
-    return true;
 }
