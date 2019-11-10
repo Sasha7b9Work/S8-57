2a21dfc4d400ad22ad0d73fbf5a999c86820108b
Sasha7b9 <Sasha7b9@tut.by>
2019-11-10 20:38:36 +0300

    Подготовка к новому алгоритму хранения данных в RAM

--- sources/Device/src/Hardware/Memory/MemoryRAM.cpp
+++ sources/Device/src/Hardware/Memory/MemoryRAM.cpp
@@ -5,3 +5,21 @@
 
 
 int16 MemoryRAM::currentSignal = 0;
+
+
+void MemoryRAM::Save(const DataSettings *ds)
+{
+
+}
+
+
+bool MemoryRAM::Read(DataSettings **ds, uint num)
+{
+    return false;
+}
+
+
+uint MemoryRAM::NumberDatas()
+{
+    return 0;
+}
--- sources/Device/src/Hardware/Memory/MemoryRAM.h
+++ sources/Device/src/Hardware/Memory/MemoryRAM.h
@@ -1,8 +1,17 @@
 #pragma once
 
 
+struct DataSettings;
+
+
 struct MemoryRAM
 {
     /// Номер текущего с конца сигнала. 0 - последний считанный сигнал, 1 - предпоследний считанный сигнал.
     static int16 currentSignal;
+    /// Добавить вновь считанные данные
+    static void Save(const DataSettings *ds);
+    /// Считать информацию о num с конца сохранённых данных. 0 - последние данные, 1 - предпоследние
+    static bool Read(DataSettings **ds, uint num = 0);
+    /// Возвращает количество записей в памяти
+    static uint NumberDatas();
 };
