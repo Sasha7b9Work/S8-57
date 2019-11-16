c98a70e33cae89c5e5219f280a8d66803854c090
Sasha7b9 <Sasha7b9@tut.by>
2019-11-16 17:40:25 +0300

    GUI Генератор. Работает установка амплитуды

--- sources/VS/Device/src/FPGA/FPGA_win.cpp
+++ sources/VS/Device/src/FPGA/FPGA_win.cpp
@@ -37,15 +37,13 @@ static float NextNoise()
 
 static bool GenerateNormalModeData(Chan::E ch, uint8 data[FPGA::MAX_NUM_POINTS])
 {
-    float amplitude = TuneGeneratorDialog::amplitude * 120;
+    float amplitude = TuneGeneratorDialog::amplitude / MathFPGA::RShift2Abs(1, set.ch[ch].range) * 315;
     float offset = TuneGeneratorDialog::offset;
-    float frequency = TuneGeneratorDialog::frequency;
-
-    float d = frequency * MathFPGA::TShift2Abs(1, set.time.base);
+    float frequency = TuneGeneratorDialog::frequency * MathFPGA::TShift2Abs(1, set.time.base);
 
     for (uint i = 0; i < FPGA::MAX_NUM_POINTS; i++)
     {
-        float value = offset + VALUE::AVE + amplitude * (sinf(2 * Math::PI_F * i * d));
+        float value = offset + VALUE::AVE + amplitude * (sinf(2 * Math::PI_F * i * frequency));
 
         LIMITATION(value, static_cast<float>(VALUE::MIN), static_cast<float>(VALUE::MAX));
 
