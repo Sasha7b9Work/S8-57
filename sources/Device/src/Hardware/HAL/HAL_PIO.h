044179c06debe96c0c3c6e2d0d37082698ab4759
Sasha7b9 <Sasha7b9@tut.by>
2019-12-17 08:18:54 +0300

    *

--- sources/Device/src/Hardware/HAL/HAL_PIO.h
+++ sources/Device/src/Hardware/HAL/HAL_PIO.h
@@ -56,10 +56,10 @@
 #define PIN_HCD_DM      HPort::_B, HPin::_14
 #define PIN_HCD_DP      HPort::_B, HPin::_15
 
-#define PIN_MODE0       HPort::_A, HPin::_7
-#define PIN_MODE1       HPort::_C, HPin::_4
-#define PIN_READY       HPort::_G, HPin::_12
-#define PIN_FL0         HPort::_D, HPin::_5
+#define PIN_MODE0       HPort::_A, HPin::_7     /* CS_DISP */
+#define PIN_MODE1       HPort::_C, HPin::_4     /* SEL_DISP */
+#define PIN_READY       HPort::_G, HPin::_12    /* */
+#define PIN_FL0         HPort::_D, HPin::_5     /* WR */
 #define PIN_D0          HPort::_D, HPin::_14
 #define PIN_D1          HPort::_D, HPin::_15
 #define PIN_D2          HPort::_D, HPin::_0
