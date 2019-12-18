ad4a3e21158637015da034858743f3a5b0ddd4c0
Sasha7b9 <Sasha7b9@tut.by>
2019-12-18 21:03:23 +0300

    Скорректировано количество доступной для FrameP2P памяти

--- sources/Device/src/Data/Heap.cpp
+++ sources/Device/src/Data/Heap.cpp
@@ -7,8 +7,8 @@
 static const uint SIZE_BUFFER = 79 * 1024;
 static uint8 buffer[SIZE_BUFFER];
 
-//                                                                                  Chan::Count   PeakDetMode::Enabled
-static const uint SIZE_FRAME_P2P = sizeof(DataSettings) + ENumPointsFPGA::MAX_NUM *      2      *           2;
+//                                                                                  Chan::Count Пиковый детектор не учитываем, потому что при пиковом детекторе максимальная длина памяти в точках составляет половину от доступной
+static const uint SIZE_FRAME_P2P = sizeof(DataSettings) + ENumPointsFPGA::MAX_NUM *      2;
 
 static const uint SIZE_STORAGE_RAM = SIZE_BUFFER - SIZE_FRAME_P2P;
 
