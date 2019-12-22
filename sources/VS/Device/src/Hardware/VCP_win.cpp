4909915cc31598cb57c6de9060aae9b91d3804e9
Sasha7b9 <Sasha7b9@tut.by>
2019-12-22 16:27:29 +0300

    *

--- sources/VS/Device/src/Hardware/VCP_win.cpp
+++ sources/VS/Device/src/Hardware/VCP_win.cpp
@@ -6,28 +6,28 @@
 PCD_HandleTypeDef  handlePCD;
 
 
-bool   DVCP::connectedToUSB = false;
-bool   DVCP::cableUSBisConnected = false;
+bool VCP::connectedToUSB = false;
+bool VCP::cableUSBisConnected = false;
 
 
-void DVCP::Init()
+void VCP::Init()
 {
 
 }
 
-void *DVCP::HandlePCD()
+void *VCP::HandlePCD()
 {
     return static_cast<void *>(&handlePCD);
 }
 
 
-void DVCP::SendDataAsynch(const uint8 *text, uint)
+void VCP::SendDataAsynch(const uint8 *text, uint)
 {
     ConsoleSCPI::Self()->AddText(reinterpret_cast<const char *>(text));
 }
 
 
-void DVCP::SendStringAsynch(const char *message)
+void VCP::SendStringAsynch(const char *message)
 {
     String text(">>> %s", message);
     ConsoleSCPI::Self()->AddText(text.c_str());
