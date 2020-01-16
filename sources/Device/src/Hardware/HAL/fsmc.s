525f6e8a2f66a64414a17a782e139ab762fff654
Sasha7b9 <Sasha7b9@tut.by>
2020-01-16 10:46:15 +0300

    refact

--- sources/Device/src/Hardware/HAL/fsmc.s
+++ sources/Device/src/Hardware/HAL/fsmc.s
@@ -1,23 +1,25 @@
 PERIPH_BASE      EQU 0x40000000
 AHB1PERIPH_BASE  EQU (PERIPH_BASE + 0x00020000)
+    
+SHIFT_IDR       EQU 16
+SHIFT_BSRR      EQU 24
 
 GPIOD_BASE       EQU (AHB1PERIPH_BASE + 0x0C00)
 GPIOD            EQU GPIOD_BASE
-GPIOD_BSRR       EQU GPIOD + 24
 
 GPIOA_BASE       EQU (AHB1PERIPH_BASE + 0x0000)
 GPIOA            EQU GPIOA_BASE
-GPIOA_IDR        EQU GPIOA + 16
 
 GPIO_PIN_0       EQU 0x01
 GPIO_PIN_1       EQU 0x02
 GPIO_PIN_2       EQU 0x04
 GPIO_PIN_3       EQU 0x08
 GPIO_PIN_4       EQU 0x10
-GPIO_PIN_5       EQU 0x20
+GPIO_PIN_5       EQU (GPIO_PIN_0 << 5)
 GPIO_PIN_4_5     EQU GPIO_PIN_4 + GPIO_PIN_5
 GPIO_PIN_6       EQU 0x40
 GPIO_PIN_7       EQU 0x80
+GPIO_PIN_12      EQU (GPIO_PIN_0 << 12)
     
               AREA |.text|, CODE, READONLY
               
@@ -28,15 +30,17 @@ CycleSend PROC
     
 ;GPIOD->BSRR = GPIO_PIN_5 << 16
     MOV R0, #GPIO_PIN_5 << 16
-    LDR R1, =GPIOD_BSRR
+    LDR R1, =GPIOD + SHIFT_BSRR
     STR R0, [R1]
     
 ;while(GPIOA->IDR & GPIO_PIN_7)
 cycle1
-    LDR R0, =GPIOA_IDR
+    LDR R0, =GPIOA + SHIFT_IDR
     LDR R0, [R0]
     ANDS R0, R0, #GPIO_PIN_7
     BNE   cycle1
+    
+;GPIOG->BSRR = GPIO_PIN_12 << 16
     
     POP {R0-R1, pc}
                   
