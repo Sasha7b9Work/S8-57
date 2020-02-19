1d2a1e2068f44fd7b8c49ecc079cc1dd1f314ec0
Sasha7b9 <Sasha7b9@tut.by>
2020-02-19 14:28:41 +0300

    *

--- sources/Device/src/Osci/Osci.cpp
+++ sources/Device/src/Osci/Osci.cpp
@@ -162,13 +162,21 @@ bool Osci::ProcessFlagReady()
 
 void Osci::ReadPointP2P()
 {
-    if (FrameP2P::IsCorrect() && FPGA::IsRunning() && HAL_PIO::Read(PIN_P2P))
+    bool isCorrect = FrameP2P::IsCorrect();
+    bool isRunning = FPGA::IsRunning();
+    bool isRead = HAL_PIO::Read(PIN_P2P);
+
+    LOG_WRITE("correct %d, running %d, read %d", isCorrect, isRunning, isRead);
+
+    if (isCorrect && isRunning && isRead)
     {
         HAL_BUS::FPGA::SetAddrData(RD::DATA_A, RD::DATA_A + 1);
         BitSet16 dataA(HAL_BUS::FPGA::ReadA0(), HAL_BUS::FPGA::ReadA1());
 
         HAL_BUS::FPGA::SetAddrData(RD::DATA_B, RD::DATA_B + 1);
         BitSet16 dataB(HAL_BUS::FPGA::ReadA0(), HAL_BUS::FPGA::ReadA1());
+
+        LOG_WRITE("—читаны точки");
 
         FrameP2P::AddPoint(dataA, dataB);
     }
