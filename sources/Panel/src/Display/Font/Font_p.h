c7ac9d6ab82eb3dba3c98a9a5ad3fdd5fc39b607
Sasha7b9 <Sasha7b9@tut.by>
2020-02-25 15:29:48 +0300

    detrash

--- sources/Panel/src/Display/Font/Font_p.h
+++ sources/Panel/src/Display/Font/Font_p.h
@@ -41,7 +41,6 @@ public:
     static uint8 GetHeight();
     static bool RowNotEmpty(uint8 symbol, int row);
     static bool BitIsExist(uint8 symbol, int row, int bit);
-    static void SetMinWidth(uint8 width);
     /// Устанавливает количество пустых пикселей между символами.
     static void SetSpacing(int spacing);
 };
