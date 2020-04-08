bc20409588d0e6ea9f4ded3aeec05a5eeeccd72d
Sasha7b9 <Sasha7b9@tut.by>
2020-04-08 12:11:16 +0300

    detrash

--- sources/DLoader/src/Display/Painter_stm.cpp
+++ sources/DLoader/src/Display/Painter_stm.cpp
@@ -18,27 +18,3 @@ void Painter::EndScene()
 {
     HAL_BUS::Panel::Send(Command::Paint_EndScene);
 }
-
-
-void Painter::DrawTesterData(uint8 mode, Color color, const uint16 *x, const uint8 *y)
-{
-    Buffer buffer(3 + TESTER_NUM_POINTS * 2 + TESTER_NUM_POINTS);
-    buffer.data[0] = Command::Paint_TesterLines;
-    buffer.data[1] = mode;
-    buffer.data[2] = color.value;
-
-    uint16 *pointer16 = reinterpret_cast<uint16 *>(buffer.data + 3);
-    for (int i = 0; i < TESTER_NUM_POINTS; i++)
-    {
-        *pointer16++ = x[i];
-    }
-
-    uint8 *pointer8 = reinterpret_cast<uint8 *>(pointer16);
-    for (int i = 0; i < TESTER_NUM_POINTS; i++)
-    {
-        *pointer8++ = y[i];
-    }
-
-    HAL_BUS::Panel::Send(buffer.data, buffer.Size());
-}
-
