480c52c331772fc1185c26742c57a5210e70428e
Sasha7b9 <Sasha7b9@tut.by>
2020-06-16 15:30:29 +0300

    detrash

--- sources/Device/src/Osci/AutoSearcher.cpp
+++ sources/Device/src/Osci/AutoSearcher.cpp
@@ -1,84 +1,99 @@
 #include "defines.h"
+#include "log.h"
 #include "Display/Painter.h"
 #include "Display/Primitives.h"
 #include "FPGA/FPGA.h"
-#include "FreqMeter/FreqMeter.h"
 #include "Hardware/Timer.h"
-#include "Hardware/HAL/HAL.h"
 #include "Osci/Osci.h"
-#include "Utils/Values.h"
-
-
-// Попытка найти сигнал на канале ch. Если попытка удалась, то найденные параметры загружаются в прибор
-static bool FindSignal(Chan::E ch, TBase::E *tBase, Range::E *range);
-
-// Находит частоту сигнала, поданного на вход ch
-static bool FindFrequency(Chan::E ch, float *outFreq);
-//static bool FindFrequency(Chan::E ch, Range::E range, float *outFreq);
-
-// Находит одну синхронизацию и возвращает true в случае успеха и range, при котором она найдена
-//static bool FindOneSync(Chan::E ch, Range::E range, uint timeWait);
-
-// Ищет две синхронизации во временном промежутке timeWait.
-// Если не найдено не одной, возращает -1
-// Если найдена только одна возрващает 0
-// Если найдены две, возвращает время в микросекундах между ними (но не менее 1)
-static uint FindTwoSync(Chan::E ch, Range::E range, uint timeWait);
-
-// Находит масштаб по вертикаил
-//static Range::E FindRange(Chan::E ch);
-
-// Рассчитывает TBase, необходимый для отображения задданой частоты
-static TBase::E CalculateTBase(float frequency);
-
-// Заслать соответствующие настройки в частотомер
-//static void TuneFreqMeter();
+#include "Utils/Math.h"
 
 
 static void DisplayUpdate();
 
+// Нахоит сигнал на канале ch. Возвращает true, если сигнал найден и параметры сигнала в tBase, range
+static bool FindSignal(Chan::E ch, TBase::E *tBase, Range::E *range);
 
-void Osci::_RunAutoSearch()
+// Сжать сигнал, если не влазит в экран по вертикали
+static void ToScaleChannel(Chan::E ch);
+
+// Находит частоту на канале ch
+static bool FindFrequency(Chan::E ch, float *outFreq);
+static bool FindFrequencyForRanges(Chan::E ch, uint timeWaitMS, float *outFreq);
+static bool FindFrequencyForRange(Chan::E ch, Range::E range, uint timeWaitMS, float *outFreq);
+
+// Ожидает импульса синхронизации в течение timeWaitMS миллисекунд и возвращает true, если синхронизация пришла
+static bool WaitSync(uint timeWaitMS);
+
+// Рассчитывает TBase, необходимый для отображения задданой частоты
+static TBase::E CalculateTBase(float frequency);
+
+
+namespace FrequencyMeter
+{
+    // Найти частоту по счётчику частоты
+    void TuneForFind();
+    
+    namespace State
+    {
+        // Сохранить состояние частотомера перед нашими измерениями
+        void Store();
+
+        // Восстановить состояние частотомера после наших измерений
+        void Restore();
+
+        bool enabled = S_FREQ_METER_ENABLED;
+        FreqMeter::TimeCounting::E counting = S_FREQ_TIME_COUNTING;
+        FreqMeter::FreqClc::E clc = S_FREQ_FREQ_CLC;
+        FreqMeter::NumberPeriods::E periods = S_FREQ_NUMBER_PERIODS;
+    }
+}
+
+
+void Osci::RunAutoSearch()
 {
     Settings old = set;
 
-    TBase::E tBase = S_TIME_BASE;;
+    TBase::E tBase = S_TIME_BASE;
     Range::E rangeA = S_RANGE_A;
     Range::E rangeB = S_RANGE_B;
 
-    Chan::E source = Chan::Count;
-
     if (FindSignal(ChanA, &tBase, &rangeA))
     {
-        source = ChanA;
+        set = old;
+        S_TIME_BASE = tBase;
+        S_RANGE_A = rangeA;
+        TrigSource::Set(ChanA);
+
+        Osci::Init();
+
+        ToScaleChannel(ChanA);
+        ToScaleChannel(ChanB);
     }
-    else if (FindSignal(ChanB, &tBase, &rangeB))
+    else if (FindSignal(ChanA, &tBase, &rangeB))
     {
-        source = ChanB;
+        set = old;
+        S_TIME_BASE = tBase;
+        S_RANGE_B = rangeB;
+        TrigSource::Set(ChanB);
+
+        Osci::Init();
+
+        ToScaleChannel(ChanA);
+        ToScaleChannel(ChanB);
     }
     else
     {
         DISPLAY_SHOW_WARNING("Сигнал не обнаружен");
+
+        set = old;
+        Osci::Init();
     }
-
-    set = old;
-
-    S_TIME_BASE = tBase;
-    S_RANGE_A = rangeA;
-    S_RANGE_B = rangeB;
-    
-    if (source != Chan::Count)
-    {
-        S_TRIG_SOURCE = source;
-    }
-
-    Osci::Init();
 
     FreqMeter::Init();
 }
 
 
-static bool FindSignal(Chan::E ch, TBase::E *tBase, Range::E *)
+static bool FindSignal(Chan::E ch, TBase::E *tBase, Range::E *range)
 {
     float frequency = 0.0F;
 
@@ -86,7 +101,7 @@ static bool FindSignal(Chan::E ch, TBase::E *tBase, Range::E *)
     {
         *tBase = CalculateTBase(frequency);
 
-        //Range::Set(ch, FindRange(ch));
+        *range = Range::_500mV;
 
         return true;
     }
@@ -97,6 +112,7 @@ static bool FindSignal(Chan::E ch, TBase::E *tBase, Range::E *)
 
 static bool FindFrequency(Chan::E ch, float *outFreq)
 {
+    Osci::Stop();
     ModeCouple::Set(ch, ModeCouple::AC);
     TrigSource::Set(ch);
     TrigStartMode::Set(TrigStartMode::Wait);
@@ -104,149 +120,127 @@ static bool FindFrequency(Chan::E ch, float *outFreq)
     TBase::Set(TBase::_100ns);
     TShift::Set(0);
     RShift::Set(ch, 0);
+    S_TRIG_INPUT = TrigInput::Full;
+    TrigInput::Load();
 
-    for (int range = static_cast<int>(Range::_2mV); range < Range::Count; range++)
+    if (FindFrequencyForRanges(ch, 150, outFreq))
     {
-        DisplayUpdate();
+        return true;
+    }
 
-        uint timeSync = FindTwoSync(ch, static_cast<Range::E>(range), 150);
-
-        if (timeSync == static_cast<uint>(-1))                                  // Не найдено синхронизации - переходим к следующему
-        {
-            continue;
-        }
-        else if (timeSync == 0)
-        {
-            timeSync = FindTwoSync(ch, static_cast<Range::E>(range), 1500);
-        }
-        else
-        {
-            FreqMeter::ClearMeasure();
-
-            Osci::Start(false);
-
-            do 
-            {
-                FPGA::Flag::Read();
-            } while (!FreqMeter::FrequencyIsFound());
-
-            *outFreq = FreqMeter::GetFrequency();
-        }
+    if (FindFrequencyForRanges(ch, 1200, outFreq))
+    {
+        return true;
     }
 
     return false;
 }
 
 
-//static bool FindFrequency(Chan::E ch, Range::E range, float *outFreq)
-//{
-//    /*
-//        Находим сигнал с помощью частотомера
-//        1. Устанавливаем один периода в параметрах частотомера
-//        2. Читаем счётчики, тем самым обнуляя их.
-//        3. Даём запуск.
-//        4. Ждём, пока появится флаг частоты или периода
-//    */
-//
-//    Settings old = set;
-//
-//    Osci::Stop();
-//    Range::Set(ch, range);
-//
-//    Timer::PauseOnTime(500);
-//
-//    TuneFreqMeter();
-//    
-//    FreqMeter::ClearMeasure();
-//
-//    Osci::Start(false);
-//
-//    uint timeStart = TIME_MS;
-//
-//    do
-//    {
-//        FPGA::ReadFlag();
-//        DisplayUpdate();
-//    } while (!FreqMeter::FrequencyIsFound() && (TIME_MS - timeStart < 2000));
-//
-//    *outFreq = FreqMeter::GetFrequency();
-//
-//    set = old;
-//
-//    return FreqMeter::FrequencyIsFound();
-//}
-
-
-//Range::E FindRange(Chan::E)
-//{
-//    return Range::Count;
-//}
-
-
-//static void TuneFreqMeter()
-//{
-//    S_FREQ_METER_ENABLED = true;
-//    S_FREQ_TIME_COUNTING = FreqMeter::TimeCounting::_1s;
-//    S_FREQ_FREQ_CLC = FreqMeter::FreqClc::_100MHz;
-//    S_FREQ_NUMBER_PERIODS = FreqMeter::NumberPeriods::_1;
-//
-//    FreqMeter::FPGA::LoadSettings();
-//    FreqMeter::FPGA::ResetCounterFreq();
-//    FreqMeter::FPGA::ResetCounterPeriod();
-//
-//    FPGA::Flag::Clear();
-//}
-
-
-static TBase::E CalculateTBase(float frequency)
+static bool FindFrequencyForRanges(Chan::E ch, uint timeWaitMS, float *outFreq)
 {
-    struct TimeStruct
+    bool result = false;
+
+    FrequencyMeter::State::Store();
+
+    FrequencyMeter::TuneForFind();
+
+    for (int range = static_cast<int>(Range::_20V); range > 0; range--)
     {
-        float       frequency;  // Частота, ниже которой нужно устанавливать tBase
-        TBase::E    tBase;
-    };
+        float frequency1 = 0.0F;
 
-    static const TimeStruct times[] =
-    {
-        {10.0F,  TBase::_10ms},
-        {30.0F,  TBase::_5ms},
-        {80.0F,  TBase::_2ms},
-
-        {100.0F, TBase::_1ms},
-        {300.0F, TBase::_500us},
-        {800.0F, TBase::_200us},
-
-        {1e3F,   TBase::_100us},
-        {3e3F,   TBase::_50us},
-        {8e3F,   TBase::_20us},
-
-        {10e3F,  TBase::_10us},
-        {30e3F,  TBase::_5us},
-        {80e3F,  TBase::_2us},
-
-        {100e3F, TBase::_1us},
-        {300e3F, TBase::_500ns},
-        {800e3F, TBase::_200ns},
-
-        {1e6F,   TBase::_100ns},
-        {3e6F,   TBase::_50ns},
-        {8e6F,   TBase::_20ns},
-
-        {10e6F,  TBase::_10ns},
-        {30e6F,  TBase::_5ns},
-        {250e6F, TBase::_2ns},
-        {0.0F,   TBase::Count}
-    };
-
-    for (int i = 0; times[i].tBase != TBase::Count; i++)
-    {
-        if (frequency < times[i].frequency)
+        if (FindFrequencyForRange(ch, static_cast<Range::E>(range), timeWaitMS, &frequency1))
         {
-            return times[i].tBase;
+            float frequency2 = 0.0F;
+
+            if (FindFrequencyForRange(ch, static_cast<Range::E>(range - 1), timeWaitMS, &frequency2))
+            {
+                if (Math::FloatsIsEquals(frequency1, frequency2, 0.05F))
+                {
+                    *outFreq = (frequency1 + frequency2) / 2.0F;
+                    result = true;
+                    break;
+                }
+            }
         }
     }
 
-    return TBase::_2ns;
+    FrequencyMeter::State::Restore();
+
+    return result;
+}
+
+
+static bool FindFrequencyForRange(Chan::E ch, Range::E range, uint timeWaitMS, float *outFreq)
+{
+    DisplayUpdate();
+
+    Range::Set(ch, range);
+
+    Timer::PauseOnTime(50);
+
+    FPGA::Flag::Clear();
+
+    Osci::Start(false);
+
+    if (WaitSync(timeWaitMS))
+    {
+        do
+        {
+            FPGA::Flag::Read(false);
+        } while (!FPGA::Flag::FreqReady());
+
+        BitSet32 counterFreq = FreqMeter::FPGA::ReadCounterFreq();
+
+        if (counterFreq.word > 10)
+        {
+            *outFreq = counterFreq.word * 10.0F;
+        }
+        else
+        {
+            while (!FPGA::Flag::PeriodReady())
+            {
+                FPGA::Flag::Read(false);
+            }
+
+            BitSet32 counterPeriod = FreqMeter::FPGA::ReadCounterPeriod();
+
+            *outFreq = 1.0F / (counterPeriod.word * 10e-9F);
+        }
+
+        return true;
+    }
+
+    return false;
+}
+
+
+void FrequencyMeter::TuneForFind()
+{
+    S_FREQ_METER_ENABLED = true;
+    S_FREQ_TIME_COUNTING = FreqMeter::TimeCounting::_100ms;
+    S_FREQ_FREQ_CLC = FreqMeter::FreqClc::_100MHz;
+    S_FREQ_NUMBER_PERIODS = FreqMeter::NumberPeriods::_1;
+
+    FreqMeter::FPGA::LoadSettings();
+}
+
+
+static bool WaitSync(uint timeWaitMS)
+{
+    uint start = TIME_MS;           // Время начала ожидания
+
+    while (!FPGA::Flag::TrigReady())
+    {
+        if (TIME_MS - start > timeWaitMS)
+        {
+            return false;
+        }
+
+        FPGA::Flag::Read();
+    }
+
+    return true;
 }
 
 
@@ -271,7 +265,81 @@ static void DisplayUpdate()
 }
 
 
-static uint FindTwoSync(Chan::E /*ch*/, Range::E /*range*/, uint /*timeWait*/)
+static void ToScaleChannel(Chan::E)
 {
-    return 0;
+
+}
+
+
+static TBase::E CalculateTBase(float frequency)
+{
+    struct TimeStruct
+    {
+        float       frequency;  // Частота, ниже которой нужно устанавливать tBase
+        TBase::E    tBase;
+    };
+
+    static const TimeStruct times[] =
+    {
+        {2.0F,   TBase::_200ms},
+        {3.0F,   TBase::_100ms},
+        {5.0F,   TBase::_50ms},
+        {30.0F,  TBase::_20ms},
+        {60.0F,  TBase::_5ms},
+        {80.0F,  TBase::_2ms},
+
+        {300.0F, TBase::_1ms},
+        {600.0F, TBase::_500us},
+        {800.0F, TBase::_200us},
+
+        {3e3F,   TBase::_100us},
+        {6e3F,   TBase::_50us},
+        {8e3F,   TBase::_20us},
+
+        {30e3F,  TBase::_10us},
+        {60e3F,  TBase::_5us},
+        {80e3F,  TBase::_2us},
+
+        {300e3F, TBase::_1us},
+        {600e3F, TBase::_500ns},
+        {800e3F, TBase::_200ns},
+
+        {3e6F,   TBase::_100ns},
+        {6e6F,   TBase::_50ns},
+        {15e6F,  TBase::_20ns},
+
+        {30e6F,  TBase::_10ns},
+        {60e6F,  TBase::_5ns},
+        {0.0F,   TBase::Count}
+    };
+
+    for (int i = 0; times[i].tBase != TBase::Count; i++)
+    {
+        if (frequency < times[i].frequency)
+        {
+            return times[i].tBase;
+        }
+    }
+
+    return TBase::_2ns;
+}
+
+
+void FrequencyMeter::State::Store()
+{
+    enabled = S_FREQ_METER_ENABLED;
+    counting = S_FREQ_TIME_COUNTING;
+    clc = S_FREQ_FREQ_CLC;
+    periods = S_FREQ_NUMBER_PERIODS;
+}
+
+
+void FrequencyMeter::State::Restore()
+{
+    S_FREQ_METER_ENABLED = enabled;
+    S_FREQ_TIME_COUNTING = counting;
+    S_FREQ_FREQ_CLC = clc;
+    S_FREQ_NUMBER_PERIODS = periods;
+
+    FreqMeter::Init();
 }
