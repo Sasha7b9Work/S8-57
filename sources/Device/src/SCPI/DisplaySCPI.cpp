d5f116d398b5281dc084865dbedc9b4c4b48d7a7
Sasha7b9 <Sasha7b9@tut.by>
2020-06-25 22:32:59 +0300

    *

--- sources/Device/src/SCPI/DisplaySCPI.cpp
+++ sources/Device/src/SCPI/DisplaySCPI.cpp
@@ -6,22 +6,16 @@
 #include <cstring>
 
 
-// :DISPLAY:MAPPING
-static const char *FuncMapping(const char *);
-static bool TestMapping();
-static void HintMapping(String *);
 // :DISPLAY:AVERAGES
 static const char *FuncAverages(const char *);
 static bool TestAverages();
 static void HintAverages(String *);
 
 
-static const char *const mapping[] =
-{
-    " LINES",
-    " DOTS",
-    ""
-};
+// :DISPLAY:MAPPING
+static const char *FuncMapping(const char *);
+static bool TestMapping();
+static void HintMapping(String *);
 
 
 static const char *const averages[] =
@@ -39,52 +33,20 @@ static const char *const averages[] =
 };
 
 
+static const char *const mapping[] =
+{
+    " LINES",
+    " DOTS",
+    ""
+};
+
+
 const StructSCPI SCPI::display[] =
 {
     SCPI_LEAF(":AVERAGE",  FuncAverages, TestAverages, "Number of averages",     HintAverages),
     SCPI_LEAF(":MAPPING",  FuncMapping, TestMapping,   "Signal display control", HintMapping),
     SCPI_EMPTY()
 };
-
-
-static const char *FuncMapping(const char *buffer)
-{
-    SCPI_REQUEST(SCPI::SendAnswer(mapping[S_DISP_MAPPING]));
-
-    SCPI_PROCESS_ARRAY(mapping, S_DISP_MAPPING = static_cast<DisplayMapping::E>(i));
-}
-
-
-static bool TestMapping()
-{
-    String commandLines(":DISPLAY:MAPPING LINEs%c", 0x0D);
-    String commandDots(":DISPLAY:MApping dots%c", 0x0D);
-
-    for(int i = 0; i < 10; i++)
-    {
-        SCPI_APPEND_STRING(commandLines); //-V814
-
-        if(!S_DISP_MAPPING_IS_LINES)
-        {
-            SCPI_EXIT_ERROR();
-        }
-
-        SCPI_APPEND_STRING(commandDots); //-V814
-
-        if(!S_DISP_MAPPING_IS_DOTS)
-        {
-            SCPI_EXIT_ERROR();
-        }
-    }
-
-    return true;
-}
-
-
-static void HintMapping(String *message)
-{
-    SCPI::ProcessHint(message, mapping);
-}
 
 
 static const char *FuncAverages(const char *buffer)
@@ -95,13 +57,53 @@ static const char *FuncAverages(const char *buffer)
 }
 
 
+static const char *FuncMapping(const char *buffer)
+{
+    SCPI_REQUEST(SCPI::SendAnswer(mapping[S_DISP_MAPPING]));
+
+    SCPI_PROCESS_ARRAY(mapping, S_DISP_MAPPING = static_cast<DisplayMapping::E>(i));
+}
+
+
 static void HintAverages(String *message)
 {
     SCPI::ProcessHint(message, averages);
 }
 
 
+static void HintMapping(String *message)
+{
+    SCPI::ProcessHint(message, mapping);
+}
+
+
 static bool TestAverages()
 {
     return false;
+}
+
+
+static bool TestMapping()
+{
+    String commandLines(":DISPLAY:MAPPING LINEs%c", 0x0D);
+    String commandDots(":DISPLAY:MApping dots%c", 0x0D);
+
+    for (int i = 0; i < 10; i++)
+    {
+        SCPI_APPEND_STRING(commandLines); //-V814
+
+        if (!S_DISP_MAPPING_IS_LINES)
+        {
+            SCPI_EXIT_ERROR();
+        }
+
+        SCPI_APPEND_STRING(commandDots); //-V814
+
+        if (!S_DISP_MAPPING_IS_DOTS)
+        {
+            SCPI_EXIT_ERROR();
+        }
+    }
+
+    return true;
 }
