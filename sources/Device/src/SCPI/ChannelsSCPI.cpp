25736ee902e08b6c6056255e9f9cbb14bcfbbfcd
Sasha7b9 <Sasha7b9@tut.by>
2020-09-14 14:44:31 +0300

    SCPI:CHANNEL:COUPLING не работает

--- sources/Device/src/SCPI/ChannelsSCPI.cpp
+++ sources/Device/src/SCPI/ChannelsSCPI.cpp
@@ -10,6 +10,11 @@
 #define EXTRACT_CHANNEL(x) Chan::E ch = (*(buffer - (x)) == '1') ? ChanA : ChanB;    /* (buffer - 7) указывает на номер канала - 1 или 2 */
 
 
+// :CHANNEL{1|2}:COUPLING
+static pCHAR FuncCoupling(pCHAR);
+static bool TestCoupling();
+static void HintCoupling(String *);
+
 // :CHANNEL{1|2}:DISPLAY
 static pCHAR FuncDisplay(pCHAR);
 static bool TestDisplay();
@@ -55,9 +60,10 @@ static pString rangeName[] =
 
 static const StructSCPI chan[] =
 {
-    SCPI_LEAF("DISPLAY", FuncDisplay, TestDisplay, "Turns channel display on/off", HintDisplay),
-    SCPI_LEAF("OFFSET",  FuncOffset,  TestOffset,  "",                             HintOffset),
-    SCPI_LEAF("SCALE",   FuncScale,   TestScale,   "Vertical zoom control",        HintScale),
+    SCPI_LEAF("COUPLING", FuncCoupling, TestCoupling, "",                             HintCoupling),
+    SCPI_LEAF("DISPLAY",  FuncDisplay,  TestDisplay,  "Turns channel display on/off", HintDisplay),
+    SCPI_LEAF("OFFSET",   FuncOffset,   TestOffset,   "",                             HintOffset),
+    SCPI_LEAF("SCALE",    FuncScale,    TestScale,    "Vertical zoom control",        HintScale),
     SCPI_EMPTY()
 };
 
@@ -69,6 +75,12 @@ const StructSCPI SCPI::channels[] =
     SCPI_NODE("2:", chan),
     SCPI_EMPTY()
 };
+
+
+static pCHAR FuncCoupling(pCHAR)
+{
+    return nullptr;
+}
 
 
 static pCHAR FuncDisplay(pCHAR buffer)
@@ -97,6 +109,12 @@ static pCHAR FuncScale(pCHAR buffer)
 }
 
 
+static void HintCoupling(String *)
+{
+
+}
+
+
 static void HintDisplay(String *message)
 {
     SCPI::ProcessHint(message, displays);
@@ -112,6 +130,12 @@ static void HintOffset(String *)
 static void HintScale(String *message)
 {
     SCPI::ProcessHint(message, rangeName);
+}
+
+
+static bool TestCoupling()
+{
+    return false;
 }
 
 
