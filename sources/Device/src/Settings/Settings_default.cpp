78f2222970fb9ff7eec6ba01760a695a12fc6fc3
Sasha7b9 <Sasha7b9@tut.by>
2020-09-16 23:04:49 +0300

    Теперь по умолчанию средний уровень громкости

--- sources/Device/src/Settings/Settings_default.cpp
+++ sources/Device/src/Settings/Settings_default.cpp
@@ -202,7 +202,7 @@ const Settings Settings::defaultSettings =
     // Ìåíþ ÑÅÐÂÈÑ
     {
         Calibrator::Mode::AC,           // calibratorMode
-        0,                              // soundVolume
+        2,                              // soundVolume
         false,                          // showInfoVoltage
         0,                              // timeDisableDisplay
         0                               // timeDisablePower
