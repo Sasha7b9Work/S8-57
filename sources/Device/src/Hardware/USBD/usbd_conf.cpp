768dccfc13107e1225b1be7a311a0ee4bb6070c3
Sasha7b9Work <Phuntik-it@tut.by>
2023-11-21 14:15:18 +0300

    *

--- sources/Device/src/Hardware/USBD/usbd_conf.cpp
+++ sources/Device/src/Hardware/USBD/usbd_conf.cpp
@@ -263,16 +263,6 @@ USBD_StatusTypeDef USBD_LL_Transmit(USBD_HandleTypeDef *pdev,
 }
 
 
-USBD_StatusTypeDef USBD_LL_PrepareReceive(USBD_HandleTypeDef *pdev, 
-                                          uint8_t  ep_addr,                                      
-                                          uint8_t  *pbuf,
-                                          uint16_t  size)
-{
-    HAL_PCD_EP_Receive(reinterpret_cast<PCD_HandleTypeDef *>(pdev->pData), ep_addr, pbuf, size); //-V2571
-    return USBD_OK;   
-}
-
-
 uint32_t USBD_LL_GetRxDataSize(USBD_HandleTypeDef *pdev, uint8_t  ep_addr)  
 {
     return HAL_PCD_EP_GetRxCount(reinterpret_cast<PCD_HandleTypeDef *>(pdev->pData), ep_addr); //-V2571
