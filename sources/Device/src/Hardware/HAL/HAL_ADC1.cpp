2927185c122cdc25524d24a98dbe325a2419dfd1
Sasha7b9Work <Phuntik-it@tut.by>
2026-06-11 10:46:16 +0300

    Изменили алгоритм чтения. То же самое

--- sources/Device/src/Hardware/HAL/HAL_ADC1.cpp
+++ sources/Device/src/Hardware/HAL/HAL_ADC1.cpp
@@ -9,18 +9,11 @@ namespace HAL_ADC1
 {
     // Для датчика напряжения аккумулятора
     static ADC_HandleTypeDef handle;
-    // Для конфигурации
-    static ADC_ChannelConfTypeDef config =
-    {
-        ADC_CHANNEL_2,
-        1,
-        ADC_SAMPLETIME_3CYCLES,
-        0
-    };
 
-    static void SetActiveChannel2();
-    static void SetActiveChannel9();
-    static uint ReadValue();
+    // Массив для хранения результатов: [0] - Battery (CH2), [1] - Charger (CH9)
+    static uint32_t results[2];
+
+    static void ReadBothChannels();
 }
 
 
@@ -33,68 +26,73 @@ void HAL_ADC1::Init()
 
     __ADC1_CLK_ENABLE(); //-V2571
 
+    HAL_Delay(1);
+
     HAL_PIO::Init(PIN_ADC1_IN2, HMode::Analog, HPull::No);
     HAL_PIO::Init(PIN_ADC1_IN9, HMode::Analog, HPull::No);
 
-    handle.Instance = ADC1; //-V2571
+    handle.Instance = ADC1;
     handle.Init.ClockPrescaler = ADC_CLOCKPRESCALER_PCLK_DIV2;
     handle.Init.Resolution = ADC_RESOLUTION_12B;
     handle.Init.DataAlign = ADC_DATAALIGN_RIGHT;
-    handle.Init.ScanConvMode = DISABLE;
-    handle.Init.EOCSelection = ADC_EOC_SINGLE_CONV;
+    handle.Init.ScanConvMode = ENABLE;          // ВКЛЮЧАЕМ СКАН РЕЖИМ
+    handle.Init.EOCSelection = ADC_EOC_SEQ_CONV; // Ждем завершения ВСЕЙ последовательности
     handle.Init.ContinuousConvMode = DISABLE;
     handle.Init.DMAContinuousRequests = DISABLE;
-    handle.Init.NbrOfConversion = 1;
+    handle.Init.NbrOfConversion = 2;             // ДВА преобразования в последовательности
     handle.Init.DiscontinuousConvMode = DISABLE;
     handle.Init.NbrOfDiscConversion = 0;
-    handle.Init.ExternalTrigConvEdge = ADC_EXTERNALTRIGCONVEDGE_NONE;  // Программный запуск преобразования вручную
+    handle.Init.ExternalTrigConvEdge = ADC_EXTERNALTRIGCONVEDGE_NONE;
     handle.Init.ExternalTrigConv = ADC_SOFTWARE_START;
 
     HAL_ADC_Init(&handle);
 
-    SetActiveChannel2();
+    // Настройка последовательности
+    ADC_ChannelConfTypeDef sConfig = { 0 };
+
+    // Канал 2 (Battery) - Ранк 1
+    sConfig.Channel = ADC_CHANNEL_2;
+    sConfig.Rank = 1;
+    sConfig.SamplingTime = ADC_SAMPLETIME_3CYCLES; // Можно увеличить для стабильности
+    sConfig.Offset = 0;
+    HAL_ADC_ConfigChannel(&handle, &sConfig);
+
+    // Канал 9 (Charger) - Ранк 2
+    sConfig.Channel = ADC_CHANNEL_9;
+    sConfig.Rank = 2;
+    HAL_ADC_ConfigChannel(&handle, &sConfig);
 }
 
 
 uint HAL_ADC1::ValueBattery()
 {
-    SetActiveChannel2();
-    
-    return ReadValue();
+    ReadBothChannels();
+
+    return results[0];
 }
 
 
 uint HAL_ADC1::ValueCharger()
 {
-    SetActiveChannel9();
+    ReadBothChannels();
 
-    return ReadValue();
+    return results[1];
 }
 
 
-uint HAL_ADC1::ReadValue()
+void HAL_ADC1::ReadBothChannels()
 {
     HAL_ADC_Start(&handle);
 
-    do
+    // Ожидаем завершения всех преобразований (EOC_SEQ_CONV)
+    if (HAL_ADC_PollForConversion(&handle, 100) != HAL_OK) // Таймаут 10 мс
     {
-    } while (HAL_ADC_PollForConversion(&handle, 1) != HAL_OK);
+        return;
+    }
 
-    return HAL_ADC_GetValue(&handle);
-}
+    // Читаем результаты в правильном порядке
+    results[0] = HAL_ADC_GetValue(&handle); // Результат Rank 1 (CH2)
+    results[1] = HAL_ADC_GetValue(&handle); // Результат Rank 2 (CH9)
 
-
-void HAL_ADC1::SetActiveChannel2()
-{
-    config.Channel = ADC_CHANNEL_2;
-
-    HAL_ADC_ConfigChannel(&handle, &config);
-}
-
-
-void HAL_ADC1::SetActiveChannel9()
-{
-    config.Channel = ADC_CHANNEL_9;
-
-    HAL_ADC_ConfigChannel(&handle, &config);
+    HAL_ADC_Stop(&handle);
 }
